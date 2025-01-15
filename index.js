1e51744a69fd7f5d33c872dcdec8142d8641d93a
JR3antos <jrsantosrojo@gmail.com>
2025-01-15 14:55:05 -0300

    Orbit Controls

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -9,6 +9,8 @@ scene.add(camera);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls( camera, renderer.domElement );
 
 const boxGeo = new THREE.BoxGeometry(1,1,1);
 const boxMat = new THREE.MeshStandardMaterial({
@@ -21,8 +23,10 @@ const hemiLight = new THREE.HemisphereLight(0xffffff, 0x00000f);
 scene.add(hemiLight);
 
 function animate(){
-  box.rotation.y += 0.001;
-  box.rotation.z += 0.001;
+  requestAnimationFrame(animate);
+  box.rotation.y += 0.0001;
+  box.rotation.z += 0.0001;
+  controls.update();
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
