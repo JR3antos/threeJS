9f2c724da4f973c987209357985ebbbdbf513562
JR3antos <jrsantosrojo@gmail.com>
2025-01-16 09:35:06 -0300

    optimizando modelo

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const scene = new THREE.Scene();
 const w = window.innerWidth;
 const h = window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 2;
 scene.add(camera);
 const renderer = new THREE.WebGLRenderer({ antialias: false, preserveDrawingBuffer: false});
 renderer.setSize(w, h);
@@ -15,18 +16,24 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-const loader = new GLTFLoader();
-let morphMesh;
-let modelLoaded = false;
 loader.load('./Model.glb', (gltf) => {
-    morphMesh = gltf.scene.getObjectByProperty('type', 'Mesh');
-    if (morphMesh && morphMesh.morphTargetInfluences) {
-        scene.add(morphMesh);
-        modelLoaded = true;
-    } else {
-        console.error('No morph targets found!');
-    }
+  morphMesh = gltf.scene.getObjectByProperty('type', 'Mesh');
+  if (morphMesh && morphMesh.morphTargetInfluences) {
+      // Calcular el Bounding Box
+      const box = new THREE.Box3().setFromObject(morphMesh);
+      const center = new THREE.Vector3();
+      box.getCenter(center);
+
+      // Ajustar la posición del modelo para centrarlo
+      morphMesh.position.sub(center);
+
+      scene.add(morphMesh);
+      modelLoaded = true;
+  } else {
+      console.error('No morph targets found!');
+  }
 });
+
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x00000f);
 scene.add(hemiLight);
