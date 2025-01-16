e64a898b61e6dd805072cf80187cdd27e964343d
JR3antos <jrsantosrojo@gmail.com>
2025-01-16 09:38:55 -0300

    optimizando modelo

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import { GLTFLoader } from 'jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const w = window.innerWidth;
+const w = window.innerWidth; // Reduced resolution
 const h = window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 2;
 scene.add(camera);
-const renderer = new THREE.WebGLRenderer({ antialias: false});
+const renderer = new THREE.WebGLRenderer({ antialias: false }); // Disabled AA for performance
 renderer.setSize(w, h);
 renderer.setClearColor(0xffffff);
 document.body.appendChild(renderer.domElement);
@@ -16,24 +16,16 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
+const loader = new GLTFLoader();
+let morphMesh;
 loader.load('./Model.glb', (gltf) => {
-  morphMesh = gltf.scene.getObjectByProperty('type', 'Mesh');
-  if (morphMesh && morphMesh.morphTargetInfluences) {
-      // Calcular el Bounding Box
-      const box = new THREE.Box3().setFromObject(morphMesh);
-      const center = new THREE.Vector3();
-      box.getCenter(center);
-
-      // Ajustar la posición del modelo para centrarlo
-      morphMesh.position.sub(center);
-
-      scene.add(morphMesh);
-      modelLoaded = true;
-  } else {
-      console.error('No morph targets found!');
-  }
+    morphMesh = gltf.scene.getObjectByProperty('type', 'Mesh');
+    if (morphMesh && morphMesh.morphTargetInfluences) {
+        scene.add(morphMesh);
+    } else {
+        console.error('No morph targets found!');
+    }
 });
-
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x00000f);
 scene.add(hemiLight);
@@ -45,9 +37,6 @@ const slider3 = createSlider(60);
 let lastSliderValues = [0, 0, 0];
 function animate() {
     requestAnimationFrame(animate);
-
-    if (!modelLoaded) return; // No hacer nada si el modelo aún no se ha cargado
-
     const newValues = [
         parseFloat(slider1.value),
         parseFloat(slider2.value),
@@ -56,11 +45,9 @@ function animate() {
 
     if (newValues[0] !== lastSliderValues[0] || newValues[1] !== lastSliderValues[1] || newValues[2] !== lastSliderValues[2]) {
         if (morphMesh && morphMesh.morphTargetInfluences) {
-            for (let i = 0; i < newValues.length; i++) {
-                if (i < morphMesh.morphTargetInfluences.length) {
-                    morphMesh.morphTargetInfluences[i] = newValues[i];
-                }
-            }
+            morphMesh.morphTargetInfluences[0] = newValues[0];
+            morphMesh.morphTargetInfluences[1] = newValues[1];
+            morphMesh.morphTargetInfluences[2] = newValues[2];
         }
         lastSliderValues = newValues;
     }
