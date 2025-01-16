f16c506439fe0306b7798f361cb9171f6657cee8
JR3antos <jrsantosrojo@gmail.com>
2025-01-16 09:22:43 -0300

    optimizando modelo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "three";
+import { GLTFLoader } from 'jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const w = window.innerWidth;
